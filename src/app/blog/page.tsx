8c55649ec0b107e5933cad7336700325f425e4aa
Nicholas Cerne <nicholascerne@Nicholass-MacBook-Air.local>
2024-10-17 18:20:43 -0400

    Making blog more mobile friendly

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -14,34 +14,37 @@ export default async function Blog() {
     <>
       {/* Featured Latest Post */}
       <Stack align="center" gap="64px">
-        <Flex w="80%" key={latestPost.slug} align="center" direction={{ base: 'column', md: 'row' }}>
-          
-          {/* Text Section on the Left */}
-          <Stack w="60%" pr="lg" spacing="xs">
-          <Title order={2}>Latest Post</Title>
-          <Divider/>
+        <Flex
+            w="80%"
+            key={latestPost.slug}
+            direction={{ base: 'column', md: 'row' }} // column on smaller screens, row on medium and up
+            align="center"
+            gap="md"
+          >
+            {/* Image Section on the Top for small screens, Right for larger */}
             <Anchor href={latestPost.external} target='_blank' rel='noopener noreferrer' underline='never'>
-              <Title order={3}>{latestPost.title}</Title>
+              <Image src={latestPost.image} alt={latestPost.title} width="100%" />
             </Anchor>
 
-            <Text c="dimmed" size="md" style={{ display: 'flex', alignItems: 'center' }}>
-              <IconCalendar style={{ marginRight: '5px' }} />
-              {new Intl.DateTimeFormat('en-US', { month: 'short', day: '2-digit', year: 'numeric' }).format(new Date(latestPost.date))} | {latestPost.author}
-            </Text>
-            <Text>{latestPost.summary}</Text>
-          </Stack>
+            {/* Text Section on the Bottom for small screens, Left for larger */}
+            <Stack w="60%" spacing="xs">
+              <Anchor href={latestPost.external} target='_blank' rel='noopener noreferrer' underline='never'>
+                <Title order={2} size={{ base: 'h3', sm: 'h2', md: 'h1' }}>{latestPost.title}</Title>
+              </Anchor>
 
-          {/* Image Section on the Right */}
-          <Anchor href={latestPost.external} target='_blank' rel='noopener noreferrer' underline='never'>
-            <Image src={latestPost.image} alt={latestPost.title} width="100%" />
-          </Anchor>
-        </Flex>
+              <Text c="dimmed" size="md" style={{ display: 'flex', alignItems: 'center' }}>
+                <IconCalendar style={{ marginRight: '5px' }} />
+                {new Intl.DateTimeFormat('en-US', { month: 'short', day: '2-digit', year: 'numeric' }).format(new Date(latestPost.date))} | {latestPost.author}
+              </Text>
+              <Text>{latestPost.summary}</Text>
+            </Stack>
+          </Flex>
 
         {/* Smaller Post Cards */}
         <Grid gutter="md">
           {remainingPosts.map((post) => (
             <GridCol key={post.slug} xs={12} sm={6} md={4}>
-              <Card shadow="sm" p="lg" radius="md" withBorder w="500px">
+              <Card shadow="sm" p="lg" radius="md" withBorder w={{base: "400px", lg: "500px"}}>
                 <CardSection>
                 <Anchor href={post.external} target='_blank' rel='noopener noreferrer' underline='never'>
                   <Image src={post.image} alt={post.title} height={160} />
@@ -51,7 +54,7 @@ export default async function Blog() {
                 <Stack spacing="xs" mt="sm">
                   {post.external ? (
                     <Anchor href={post.external} target='_blank' rel='noopener noreferrer' underline='never'>
-                      <Title order={3}>{post.title}</Title>
+                      <Title order={3} size={{ base: 'h3', sm: 'h2', md: 'h1' }}>{post.title}</Title>
                     </Anchor>
                   ) : (
                     <Anchor href={`/blog/${post.slug}`} underline="never">
